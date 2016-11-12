20379d0c6c7d9a0e7eda27e052ae5801b91d3ce0
Timo Lins <timo.lins@gmail.com>
2016-11-12 10:13:20 +0100

    Linting fixes

--- index.js
+++ index.js
@@ -3,19 +3,20 @@ const DIRECTION = {
   VERTICAL: 'VERICAL'
 }
 
+function requestSplit(splitDirection, cwd) {
+  window.rpc.emit('new', {
+    splitDirection,
+    cwd
+  })
+}
 
-exports.middleware = (store) => (next) => (action) => {
-  if ('SESSION_ADD_DATA' === action.type) {
-    const { data } = action;
+exports.middleware = store => next => action => {
+  if (action.type === 'SESSION_ADD_DATA') {
+    const {data} = action
     if (/(hyperlayout: command not found)|(command not found: hyperlayout)/.test(data)) {
-      const {ui} = store.getState();
-      window.rpc.emit('new', {
-        splitDirection: DIRECTION.VERTICAL,
-        cwd: ui.cwd
-      })
-    } else {
-      next(action);
+      const {ui} = store.getState()
+      requestSplit(DIRECTION.VERICAL, ui.cwd)
     }
   }
   next(action)
-};
+}
